07f252c2c798934711b35f8240b4788d76a8da13
R00tB33rMan <rootb33rman@gmail.com>
2024-05-21 10:42:04 -0400

    Cleanup and apply dynamic pool size determinator

--- src/main/java/eu/decentsoftware/holograms/api/utils/scheduler/S.java
+++ src/main/java/eu/decentsoftware/holograms/api/utils/scheduler/S.java
@@ -2,9 +2,7 @@ package eu.decentsoftware.holograms.api.utils.scheduler;
 
 import eu.decentsoftware.holograms.api.DecentHolograms;
 import eu.decentsoftware.holograms.api.DecentHologramsAPI;
-import eu.decentsoftware.holograms.api.utils.DExecutor;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.IllegalPluginAccessException;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.concurrent.Executors;
@@ -14,7 +12,9 @@ import java.util.concurrent.TimeUnit;
 public class S {
 
     private static final DecentHolograms DECENT_HOLOGRAMS = DecentHologramsAPI.get();
-    private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newScheduledThreadPool(2);
+
+    private static final int POOL_SIZE = Math.max(1, Runtime.getRuntime().availableProcessors() / 4);
+    private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newScheduledThreadPool(POOL_SIZE);
 
     public static void stopTask(int id) {
         Bukkit.getScheduler().cancelTask(id);
@@ -28,20 +28,12 @@ public class S {
         return Bukkit.getScheduler().runTaskLater(DECENT_HOLOGRAMS.getPlugin(), runnable, delay);
     }
 
-    public static BukkitTask syncTask(Runnable runnable, long interval) {
-        return Bukkit.getScheduler().runTaskTimer(DECENT_HOLOGRAMS.getPlugin(), runnable, 0, interval);
-    }
-
     public static void async(Runnable runnable) {
         EXECUTOR_SERVICE.execute(runnable);
     }
 
     public static void async(Runnable runnable, long delay) {
         EXECUTOR_SERVICE.schedule(runnable, delay * 50, TimeUnit.MILLISECONDS);
-    }
-
-    public static BukkitTask asyncTask(Runnable runnable, long interval) {
-        return Bukkit.getScheduler().runTaskTimerAsynchronously(DECENT_HOLOGRAMS.getPlugin(), runnable, 0, interval);
     }
 
     public static BukkitTask asyncTask(Runnable runnable, long interval, long delay) {
