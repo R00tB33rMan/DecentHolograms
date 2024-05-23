48f037798c45f827995e91736110e04e9a0d6910
R00tB33rMan <rootb33rman@gmail.com>
2024-05-23 15:08:48 -0400

    Make thread pool size configurable

--- src/main/java/eu/decentsoftware/holograms/api/utils/scheduler/S.java
+++ src/main/java/eu/decentsoftware/holograms/api/utils/scheduler/S.java
@@ -2,6 +2,7 @@ package eu.decentsoftware.holograms.api.utils.scheduler;
 
 import eu.decentsoftware.holograms.api.DecentHolograms;
 import eu.decentsoftware.holograms.api.DecentHologramsAPI;
+import eu.decentsoftware.holograms.api.Settings;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -13,7 +14,7 @@ public class S {
 
     private static final DecentHolograms DECENT_HOLOGRAMS = DecentHologramsAPI.get();
 
-    private static final int POOL_SIZE = Math.max(1, Runtime.getRuntime().availableProcessors() / 4);
+    private static final int POOL_SIZE = Settings.getConfig().getInt("pool-size");
     private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newScheduledThreadPool(POOL_SIZE);
 
     public static void stopTask(int id) {
